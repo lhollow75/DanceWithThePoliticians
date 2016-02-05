e900925c6c1449672ed958f56655b9e62b0b91c3
Matilde <Matilde>
2016-02-05 23:10:59 +0100

    Add the point game

--- js/javascript.js
+++ js/javascript.js
@@ -2,20 +2,47 @@ var bras_d = document.getElementById('bras_droit');
 var bras_g = document.getElementById('bras_gauche');
 var jambe_d = document.getElementById('jambe_droite');
 var jambe_g = document.getElementById('jambe_gauche');
+var affiche_niveau = document.getElementById('niveau');
+var affiche_point = document.getElementById('point');
+var affiche_touche = document.getElementById('touche');
+var affiche_touche_suivante = document.getElementById('touche_suivante');
+var affiche_commentaire = document.getElementById('commentaire');
+var affiche_chrono = document.getElementById('chrono');
 var b = document.getElementsByTagName('body')[0];
 
+
 // création d'un tableau qui rassemble les mouvements dispo dans le css (pour la fonction random notamment)
-var position_bras = [180, -135, -90, -45, 45, 90, 135, -180, -360, 360, 0];
+var position_bras = [-360, -315, -270, -225, -180, -135, -90, -45, 0, 45, 90, 135, 180, 225, 270, 315, 360];
 
 // initialisation de la position des bras (pour la touche entrée et suppr)
 var position_bras_droit = position_bras[3];
 var position_bras_gauche = position_bras[4];
+
+// Mode jeu
+var isPlaying = false;
+var listeEnchainements = [37,38,39,40];
+var first_game = [37, 38, 39, 40, 30, 38, 37];
+var touche_attendue;
+var prochaine_touche;
+var frequence = 5000;
+var ATIlAppuye;
+var enchainement = [listeEnchainements, first_game];
+var point = 0;
+var vie = 3;
 		
 
 $(function() {
 	$('#interface').css('display', 'none');
 	$('#game').css('display', 'block');
-	b.addEventListener('keyup',  keyd);
+
+	
+	b.addEventListener('keyup',  function(e){
+		if (isPlaying){
+			play(e);
+		} else {
+			keyMoves(e);
+		}
+	});
 	
 
 	// Liste des personnages présent sur le caroussel
@@ -58,28 +85,19 @@ $(function() {
 // Permet de bouger les bras selon son côté et l'angle de destination
 // Retour à la normal par la suite
 function bougeBras(elt, angle){
-
-    elt.classList.add('bras_move_' + angle);
 	
 	// Selon l'angle de destination, le temps de retour n'est pas le même, ainsi que le traitement du retour
-	if (angle>-360 && angle<360) {
-		setTimeout(function(){
-			elt.classList.remove('bras_move_' + angle);
-		},350);
-	} else {
-		setTimeout(function(){
-			elt.classList.remove('bras_move_' + angle);
-			bougeBras(elt, 0);
-		},750);
-	}
+	elt.classList.add('bras_move_' + angle);
+    setTimeout(function(){
+        elt.classList.remove('bras_move_' + angle);
+    }, (Math.abs(angle) > 180 || Math.abs(angle) < -180) ? 750 : 350);
     
 }
 
-// Activation des mouvements en fonction de la touche appuyée
-function keyd(ev) {
-	console.log(ev.keyCode);
-	
-	switch (ev.keyCode) {
+
+// Activation des mouvements en fonction de la touche appuyée, dispo en mode jeu
+function fleches(ev){ 
+	switch(ev.keyCode) {
 		case 8: // return
 			if (position_bras_droit == -135) {
 				position_bras_droit = 135;
@@ -88,9 +106,10 @@ function keyd(ev) {
 			} else {
 				position_bras_droit = position_bras_droit - 45;
 				position_bras_gauche = position_bras_gauche - 45;
-			}
+			}   
 			bougeBras(bras_g, position_bras_gauche);
             bougeBras(bras_d, position_bras_droit);
+			
 			break;
 		case 13: // Entrée
 			if (position_bras_droit == 135) {
@@ -106,30 +125,72 @@ function keyd(ev) {
             bougeBras(bras_d, position_bras_droit);
 			break;
 		case 32: // Espace
+			console.log("Saute");
+			if (isPlaying && !firstPlay) {
+				console.log("Espace");
+			} else if (isPlaying){
+				firstPlay =! firstPlay;
+				playingMode(ev);
+			} else {
+				bougeBras(bras_g, position_bras[11]);
+				bougeBras(bras_d, position_bras[5]);
+			}
 			break;
 		case 37: // Gauche
 			console.log("A Gauche");
-			bougeBras(bras_g, position_bras[6]);
-            bougeBras(bras_d, position_bras[5]);
+			bougeBras(bras_g, position_bras[11]);
+            bougeBras(bras_d, position_bras[2]);
 
 			break;
 		case 38: // Haut
 			console.log("En haut");
-			bougeBras(bras_g, position_bras[6]);
-            bougeBras(bras_d, position_bras[1]);
+			bougeBras(bras_g, position_bras[11]);
+            bougeBras(bras_d, position_bras[5]);
 
 			break;
 		case 39: // Droite
 			console.log("A Droite");
-			bougeBras(bras_g, position_bras[2]);
-            bougeBras(bras_d, position_bras[1]);
+			bougeBras(bras_g, position_bras[14]);
+            bougeBras(bras_d, position_bras[5]);
 
 			break;
 		case 40: // Bas
 			console.log("En bas");
-			bougeBras(bras_g, position_bras[4]);
-            bougeBras(bras_d, position_bras[3]);
+			bougeBras(bras_g, position_bras[9]);
+            bougeBras(bras_d, position_bras[7]);
+			break;
+		case 80: // P -> Play
+			if (isPlaying) {}
+			console.log("1: "+isPlaying);
+			isPlaying = !isPlaying;
+			if (isPlaying){
+				console.log("Play Mode");
+				console.log("2: "+isPlaying);
+				point = 0;
+				vie = 3;
+				frequence = 5000;
+				jouerLeStep(0, 0);
+			}else {
+				console.log("Quit playing mode");
+				console.log("3: "+isPlaying);
+				firstPlay = true;
+			}
+            break;
+	}
+}
 
+// Calcule d'un angle aléatoire entre -360 et 360, en multiple de 45
+function angle_rdm(){
+	var _rdm = Math.round((Math.random() * (8 - (-8)) + (-8))) * 45;
+	return _rdm;
+}
+
+// Activation des mouvements en fonction de la touche appuyée, pas dispo en mode jeu
+function keyMoves(ev) {
+	console.log(ev.keyCode);
+	
+	fleches(ev);
+	switch (ev.keyCode) {
 		case 65: // A
 			console.log("A");
 			break;
@@ -138,23 +199,101 @@ function keyd(ev) {
 			break;
 		case 82: // R -> Random positiion
 			console.log("Random");
-			bougeBras(bras_g, position_bras[Math.floor(Math.random()*position_bras.length)]);
-			bougeBras(bras_d, position_bras[Math.floor(Math.random()*position_bras.length)]);
+			bougeBras(bras_g, angle_rdm());
+			bougeBras(bras_d, angle_rdm());
 			break;
 		case 84: // T
 			console.log("Tout en haut");
-			bougeBras(bras_g, position_bras[0]);
-			bougeBras(bras_d, position_bras[7]);
+			bougeBras(bras_g, position_bras[12]);
+			bougeBras(bras_d, position_bras[4]);
 			break;
 		case 89: // Y
 			console.log("Tour complet");
-			bougeBras(bras_g, position_bras[8]);
+			bougeBras(bras_g, position_bras[16]);
 			setTimeout(function(){
-				bougeBras(bras_d, position_bras[9]);
+				bougeBras(bras_d, position_bras[0]);
 			},50);
 			break;
 		case 90: // Z
 			console.log("Z");
 			break;
 	}
+}
+
+function play(ev){
+
+    fleches(ev);
+
+    ATIlAppuye = true;
+    console.log('touche appuyée');
+
+    if (ev.keyCode === touche_attendue){
+        console.log('bravo');
+		point++;
+		affiche_point.innerHTML = point;
+    } else {
+        console.log('là');
+        IlSEstTrompe();
+    }
+
+}
+
+function jouerLeStep(pas, niveau){
+	// console.log("tab:"+enchainement);
+    niveauEnCours = enchainement[niveau];
+	affiche_niveau.innerHTML = "Niveau: "+niveau + 1;
+	// console.log(niveauEnCours);
+    touche_attendue = niveauEnCours[pas];
+	affiche_touche.innerHTML = "Appuies sur la touche : "+touche_attendue;
+	
+	if (pas + 1 < niveauEnCours.length) {
+		prochaine_touche = niveauEnCours[pas+1];
+	} else if (niveau + 1 < enchainement.length){
+		niveau_suivant = enchainement[niveau + 1];
+		prochaine_touche = niveau_suivant[0];
+	} else {
+		prochaine_touche = "";
+	}
+	affiche_touche_suivante.innerHTML = "Prochaine touche: "+prochaine_touche;
+	
+    ATIlAppuye = false;
+
+    console.log('Attendu', touche_attendue);
+    console.log('Niveau', niveau + 1);
+	var compteur = frequence/1000;
+	affiche_chrono.innerHTML = "Chrono: "+compteur;
+	var timer = setInterval(function(){
+		affiche_chrono.innerHTML = "Chrono: "+compteur;
+		compteur--;
+	}, 1000);
+
+    setTimeout(function(){
+        if (!ATIlAppuye){
+            console.log('ici');
+            IlSEstTrompe();
+        }
+		if (vie >= 0) {
+			if (pas + 1 < niveauEnCours.length) {
+				jouerLeStep(pas + 1, niveau);
+			} else if (niveau + 1 < enchainement.length){
+				affiche_commentaire.innerHTML = "Passage au niveau supérieur";
+				frequence = frequence - 1000;
+				jouerLeStep(0, niveau+1)
+			} else {
+				console.log("Fin du jeu !");
+				affiche_commentaire.innerHTML = "Félicitation";
+			}
+		} else {
+			console.log("Game Over");
+			affiche_commentaire.innerHTML = "Game Over";
+			clearInterval(timer);
+		}
+        
+    }, frequence);
+}
+
+function IlSEstTrompe(){
+    console.log('Erreur de mouvement');
+	vie --;
+	affiche_commentaire.innerHTML = "Erreur !";
 }
